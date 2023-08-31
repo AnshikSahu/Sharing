f355081314fce8b4a0e24e603d7c154c888af86d
Anshik Sahu <97736240+AnshikSahu@users.noreply.github.com>
2023-08-31 23:27:14 +0530

    Update p2p_latest.py

--- p2p_latest.py
+++ p2p_latest.py
@@ -3,7 +3,8 @@ import threading
 
 global lines 
 lines = {}
-
+lim=950
+reply='0'
 # Function to handle sending messages to clients
 def send_messages(client_socket):
     while True:
@@ -12,31 +13,36 @@ def send_messages(client_socket):
 
 def handle_client(client_socket, client_address):
     print("Connected by", client_address)
-
+    global reply
+    global lines
+    global lim
     # Start the thread for sending messages
     # send_thread = threading.Thread(target=send_messages, args=(client_socket,))
     # send_thread.start()
 
-    while True:
+    while reply=='0':
         # Receive data from the client
-        data = client_socket.recv(1024).decode('utf-8')
+        data = client_socket.recv(4096).decode('utf-8')
         if not data:
             break
-        print("Received from", client_address, ":", data)
+        #print("Received from", client_address, ":", data)
         line_no, line, _ = map(str,data.split('\n')) 
         line_no = int(line_no)
         if line_no not in lines.keys():
             lines[line_no] = line
-        client_socket.sendall(data.encode())
+        if(len(lines)==lim):
+            reply='1'
+        client_socket.sendall(reply.encode())
         print("printinh length of lines ",len(lines))
-        
+
+    client_socket.close()        
 
     # Clean up the connection
     # client_socket.close()
     # print("Connection with", client_address, "closed")
 
 def main():
-    server_ip = '10.194.62.66'  # Listen on all available interfaces
+    server_ip = '10.194.28.9'  # Listen on all available interfaces
     base_port = 12345
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -56,6 +62,12 @@ def main():
 
         client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
         client_thread.start()
+        if(reply=='1'):
+            break
+    print("Done receiving")
+    # print("Closing server socket...")
+    server_socket.close()
+
         
 
 if __name__ == "__main__":
