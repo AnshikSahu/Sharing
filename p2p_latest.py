3995f2de2b73d77c1c240b06ce071be9ec3d0bce
Kashish Goel <96175365+KashishGoel24@users.noreply.github.com>
2023-09-01 18:35:20 +0200

    Update p2p_latest.py

--- p2p_latest.py
+++ p2p_latest.py
@@ -5,8 +5,10 @@ global lines
 global reply
 global lim
 lines = {}
-lim=950
+lim=1000
 reply='0'
+svayu = socket.socket()
+svayu.connect(("vayu.iitd.ac.in", 9801))
 def handle_client(client_socket, client_address):
     global lines 
     global reply
@@ -35,15 +37,15 @@ def handle_client(client_socket, client_address):
     client_socket.close()
 
 def my_client():
-    svayu = socket.socket()
-    svayu.connect(("vayu.iitd.ac.in", 9801))
+    global svayu
+    prev=-1
     while True:
         try:
             svayu.sendall(b"SENDLINE\n")
             response = svayu.recv(4096).decode('utf-8')
             if(response=="-1\n-1\n"):
                 continue
-            reply=sendline(response)
+            reply=sendline(response,prev)
             # logging.warning(reply)
             if (reply=="1"):
                 break
@@ -52,9 +54,8 @@ def my_client():
             svayu.close()
             break
     print("Thread Closed")
-    svayu.close()
 
-def sendline(data):
+def sendline(data,prev):
     global lines
     global reply
     global lim
@@ -73,7 +74,9 @@ def sendline(data):
 
 def main():
     global reply
-    server_ip = '10.194.28.9'  # Listen on all available interfaces
+    global lines
+    global svayu
+    server_ip = '10.194.60.95'  # Listen on all available interfaces
     base_port = 12345
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((server_ip, base_port))
@@ -81,11 +84,26 @@ def main():
     print("Server is listening for incoming connections...")
     my_thread = threading.Thread(target=my_client)
     my_thread.start()
-    while reply!='1':
-        client_socket, client_address = server_socket.accept()
-        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-        client_thread.start()
+    client_socket, client_address = server_socket.accept()
+    # client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+    # client_thread.start()
+    handle_client(client_socket,client_address)
+    # client_socket1, client_address1 = server_socket.accept()
+    # client_thread1 = threading.Thread(target=handle_client, args=(client_socket1, client_address1))
+    # client_thread1.start()
+    # client_socket2, client_address2 = server_socket.accept()
+    # client_thread2 = threading.Thread(target=handle_client, args=(client_socket2, client_address2))
+    # client_thread2.start() 
     print("Done receiving")
     server_socket.close()
+    print("starting to submit the lines")
+    svayu.sendall(b"SUBMIT\nKASHISH@COL334-672\n1000\n")
+    for i in range(0,1000):
+        line=f"{i}\n{lines[i+1]}\n"
+        svayu.sendall(line.encode())
+    resp=svayu.recv(4096).decode('utf-8').split()
+    print(resp)
+    svayu.close()
+    print('\n'.join(lines.values()))
 if __name__ == "__main__":
     main()
