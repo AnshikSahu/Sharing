cb78ddd8932afde46e1884d4cd1e6c5b2827f3db
Anshik Sahu <97736240+AnshikSahu@users.noreply.github.com>
2023-09-03 22:49:05 +0530

    Update p2p_latest.py

--- p2p_latest.py
+++ p2p_latest.py
@@ -1,107 +1,111 @@
 import socket
 import threading
 import logging
-# import time
+from time import sleep
 global lines 
 global reply
 global lim
-# time_start = time.time()
 lines = {}
 lim=1000
 reply='0'
 svayu = socket.socket()
-svayu.connect(("10.17.7.134", 9801))
+svayu.connect(("vayu.iitd.ac.in", 9801))
 def handle_client(client_socket, client_address):
     global lines 
     global reply
     global lim
     print("Connected by", client_address)
-    prev=-1
     while reply!='1':
         try:
-            data = client_socket.recv(4096).decode('utf-8')
+            data=""
+            while(True):
+                data_new= client_socket.recv(4096).decode('utf-8')
+                data+=data_new
+                if(not data_new or data[-1]=='\n'): break
             if not data: continue
             try:
                 line_no, line, *_ = map(str,data.split('\n')) 
                 line_no = int(line_no)
-                prev=line_no
                 if(line_no==-1): continue
-                if line_no not in lines.keys(): lines[line_no] = line
-                l=len(lines)
-                logging.warning(l)
-                if(l==lim): reply='1'
-            except: lines[prev]=lines[prev]+data
+            except: 
+                client_socket.sendall(reply.encode())
+                continue
+            if line_no not in lines.keys(): 
+                lines[line_no] = line
+                logging.warning(len(lines))
+            l=len(lines)
+            if(l==lim): reply='1'
             client_socket.sendall(reply.encode())
         except Exception as e:
             print("error: ", e)
-            print(data,reply)
+            print(data)
     print("Thread Closed")
     client_socket.close()
 
 def my_client():
     global svayu
-    prev=-1
-    svayu.sendall(b"SESSION RESET\n")
-    reply_1 = svayu.recv(4096).decode('utf-8')
     while True:
         try:
             svayu.sendall(b"SENDLINE\n")
-            response = svayu.recv(4096).decode('utf-8')
-            while (response[-1] != '\n'):
-                response += svayu.recv(4096).decode('utf-8')
-
-            if(response=="-1\n-1\n"):
-                continue
-            reply=sendline(response,prev)
-            # logging.warning(reply)
-            if (reply=="1"):
-                break
+            response=""
+            while(True):
+                response_new= svayu.recv(4096).decode('utf-8')
+                response+=response_new
+                if(not response_new or response_new[-1]=='\n'): break
         except Exception as e:
-            print("error: ", e,response)
-            svayu.close()
+            continue
+        reply=sendline(response)
+        if (reply=="1"):
             break
     print("Thread Closed")
 
-def sendline(data,prev):
+def sendline(data):
     global lines
     global reply
     global lim
     if not data: return '0'
     try:
-        line_no, line, *_ = map(str,data.split('\n')) 
+        line_no, line, *_ = map(str,data.split('\n'))
+        if(line_no=="-1"): return '0'
         line_no = int(line_no)
-        prev=line_no
-        if(line_no==-1): return '0'
-        if line_no not in lines.keys(): lines[line_no] = line
-        l=len(lines)
-        # logging.warning(l)
-        if(l==lim): reply='1'
-    except: lines[prev]=lines[prev]+data
+    except: return '0'
+    if(line_no==-1): return '0'
+    if line_no not in lines.keys(): 
+        lines[line_no] = line
+        logging.warning(len(lines))
+    l=len(lines)
+    if(l==lim): reply='1'
     return reply
 
 def main():
     global reply
     global lines
     global svayu
-    server_ip = '10.194.60.95'  # Listen on all available interfaces
+    server_ip = '10.194.25.90'  # Listen on all available interfaces
     base_port = 12345
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((server_ip, base_port))
     server_socket.listen(5)
     print("Server is listening for incoming connections...")
     my_thread = threading.Thread(target=my_client)
+    client_socket, client_address = server_socket.accept()
+    client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+    client_socket1, client_address1 = server_socket.accept()
+    client_thread1 = threading.Thread(target=handle_client, args=(client_socket1, client_address1))
+    client_thread1.start()
+    print("Starting the thread")
     my_thread.start()
-    my_thread.join()
-    # client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-    # client_thread.start()
-    # client_socket1, client_address1 = server_socket.accept()
-    # client_thread1 = threading.Thread(target=handle_client, args=(client_socket1, client_address1))
-    # client_thread1.start()
+    client_thread.start()
+    while(reply!='1'):
+        pass
     # client_socket2, client_address2 = server_socket.accept()
     # client_thread2 = threading.Thread(target=handle_client, args=(client_socket2, client_address2))
     # client_thread2.start() 
     print("Done receiving")
     server_socket.close()
+    with open('output.txt', "w") as f:
+        for i in range(0,1000):
+            f.write(lines[i]+ '\n')
     print("starting to submit the lines")
     svayu.sendall(b"SUBMIT\nKASHISH@COL334-672\n1000\n")
     for i in range(0,1000):
@@ -110,7 +114,8 @@ def main():
     resp=svayu.recv(4096).decode('utf-8').split()
     print(resp)
     svayu.close()
-    # print(time.time() - time_start)
+    client_thread1.join()
+    client_thread.join()
     # print('\n'.join(lines.values()))
 if __name__ == "__main__":
     main()
