85275fe63f228438e6ce01ad66731b4550b5780b
Kashish Goel <96175365+KashishGoel24@users.noreply.github.com>
2023-09-04 19:03:55 +0200

    Update p2p_thread.py
    with json

--- p2p_thread.py
+++ p2p_thread.py
@@ -1,51 +1,54 @@
-import socket
-import threading
-
-# Function to handle sending messages to clients
-def send_messages(client_socket):
-    while True:
-        message = input("Enter a message to send to clients: ")
-        client_socket.sendall(message.encode('utf-8'))
-
-def handle_client(client_socket, client_address):
-    print("Connected by", client_address)
-
-    # Start the thread for sending messages
-    send_thread = threading.Thread(target=send_messages, args=(client_socket,))
-    send_thread.start()
-
-    while True:
-        # Receive data from the client
-        data = client_socket.recv(1024).decode('utf-8')
-        if not data:
-            break
-        print("Received from", client_address, ":", data)
-
-    # Clean up the connection
-    client_socket.close()
-    print("Connection with", client_address, "closed")
-
+import socket		
+import logging	
+import json
+from time import sleep
+s = socket.socket()	
+port = 8001			
+s.connect(("10.194.13.44", port))
+print("connected")
 def main():
-    server_ip = '10.184.31.235'  # Listen on all available interfaces
-    base_port = 12345
+    global s
+    svayu = socket.socket()
+    reply = s.recv(4096).decode('utf-8')
+    while reply != "OK":
+        reply = s.recv(4096).decode('utf-8')
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((server_ip, base_port))
-    server_socket.listen(5)
-    print("Server is listening for incoming connections...")
-
-    client_id = 1  # For tracking clients and assigning unique ports
-
+    svayu.connect(("10.17.7.134", 9801))
+    svayu.sendall(b"SESSION RESET\n")
+    reply1=svayu.recv(4096).decode('utf-8')
     while True:
-        client_socket, client_address = server_socket.accept()
-
-        client_port = base_port + client_id
-        client_id += 1
-
-        print(f"Connection from {client_address[0]}:{client_address[1]} assigned to port {client_port}")
-
-        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-        client_thread.start()
-
+        try:
+            svayu.sendall(b"SENDLINE\n")
+            response = svayu.recv(4096).decode('utf-8')
+            if(response=="-1\n-1\n"):
+                continue
+            reply=sendline(response)
+            logging.warning(reply)
+            if (reply=="1"):
+                # received_data_json = s.recv(1024).decode()
+                # received_data = json.loads(received_data_json)
+                # print(received_data)
+                s.close()
+                break
+        except Exception as e:
+            print("error: ", e)
+            svayu.close()
+            break
+    print("Done")
+    s.close()
+    svayu.close()
+def sendline(sendtext):
+    global s
+    echo = None
+    while (echo != '0'):
+        try:
+            s.send(sendtext.encode())
+            echo =  s.recv(1024).decode()
+            if (echo=="1"):
+                s.close()
+                break
+        except:
+            print("error")
+    return echo
 if __name__ == "__main__":
     main()
