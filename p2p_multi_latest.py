2ca02622d56c80cfe69569893e2d9610a7c6f3ae
Anshik Sahu <97736240+AnshikSahu@users.noreply.github.com>
2023-09-07 15:36:17 +0530

    Update p2p_multi_latest.py

--- p2p_multi_latest.py
+++ p2p_multi_latest.py
@@ -8,10 +8,25 @@ lines = manager.dict()
 lim = 1000
 reply = Value('i', 0)
 svayu = None
-def handle_client(client_socket, client_address):
+def handle_client_done(client_socket,):
+    global reply
+    while reply.value!=1:
+        continue
+    rec=b'0'
+    while rec!=b'1':
+        try:
+            client_socket.sendall(b"1")
+            rec=client_socket.recv(4096)
+        except:
+            continue
+    client_socket.close()
+    print("Thread Closed")
+def handle_client(client_socket, client_socket_2):
     global lines 
     global reply
     global lim
+    client_reply_thread=Process(target=handle_client_done, args=(client_socket_2,))
+    client_reply_thread.start()
     while reply.value!=1:
         try:
             data=b""
@@ -20,16 +35,14 @@ def handle_client(client_socket, client_address):
                 data+=data_new
                 if(not data_new or data_new[-1]==10): break
             try:
-                line_no, line, *_ = data.split(b'\n')
+                line_no, line,_ = data.split(b'\n')
                 if(line_no==b'-1'): continue
             except: 
-                client_socket.sendall(str(reply.value).encode())
                 continue
             if line_no not in lines.keys(): 
                 lines[line_no] = (line_no+b'\n'+line+b'\n')
                 logging.warning(len(lines))
             if(len(lines)==lim): reply.value=1
-            client_socket.sendall(str(reply.value).encode())
         except Exception as e:
             print("error: ", e)
             print(data)
@@ -61,7 +74,7 @@ def sendline(data):
     global lim
     if not data: return
     try:
-        line_no, line, *_ = data.split(b'\n')
+        line_no, line,_ = data.split(b'\n')
         if(line_no==b'-1'): return
     except Exception as e: 
         print("error: "+str(e))
@@ -80,15 +93,18 @@ def main():
     global reply
     global lines
     global svayu
-    server_ip = '10.194.13.44'  # Listen on all available interfaces
+    server_ip = '10.194.1.207'  # Listen on all available interfaces
     base_port = 8001
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((server_ip, base_port))
     server_socket.listen(1)
-    server_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    base_port1 = 8002
-    server_socket1.bind((server_ip, base_port1))
-    server_socket1.listen(1)
+    server_socket_done = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket_done.bind((server_ip, base_port+1000))
+    server_socket_done.listen(1)
+    # server_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # base_port1 = 8002
+    # server_socket1.bind((server_ip, base_port1))
+    # server_socket1.listen(1)
     # server_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # base_port2 = 8003
     # server_socket2.bind((server_ip, base_port2))
@@ -96,15 +112,16 @@ def main():
     print("Server is listening for incoming connections...")
     my_thread = Process(target=my_client)
     client_socket, client_address = server_socket.accept()
-    client_thread = Process(target=handle_client, args=(client_socket, client_address))
-    client_socket1, client_address1 = server_socket1.accept()
-    client_thread1 = Process(target=handle_client, args=(client_socket1, client_address1))
+    client_socket_done, client_address_done = server_socket_done.accept()
+    client_thread = Process(target=handle_client, args=(client_socket, client_socket_done))
+    # client_socket1, client_address1 = server_socket1.accept()
+    # client_thread1 = Process(target=handle_client, args=(client_socket1, client_address1))
     # client_socket2, client_address2 = server_socket2.accept()
     # client_thread2 = Process(target=handle_client, args=(client_socket2, client_address2))
     wait_thread=Process(target=wait)
     print("Starting the thread")
     client_socket.sendall(b"OK")
-    client_socket1.sendall(b"OK")
+    # client_socket1.sendall(b"OK")
     # client_socket2.sendall(b"OK")
     svayu = socket.socket()
     svayu.connect(("10.17.6.5", 9801))
@@ -112,7 +129,7 @@ def main():
     reply1=svayu.recv(4096)
     my_thread.start()
     client_thread.start()
-    client_thread1.start()
+    # client_thread1.start()
     # client_thread2.start()
     wait_thread.start()
     wait_thread.join()
@@ -129,9 +146,9 @@ def main():
     my_thread.join()
     reply.value=1
     client_thread.join()
-    reply.value=1
-    client_thread1.join()
-    reply.value=1
+    # reply.value=1
+    # client_thread1.join()
+    # reply.value=1
     # client_thread2.join()
 if __name__ == "__main__":
     main()
